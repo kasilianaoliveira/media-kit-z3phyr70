0dfd01620df54d4bd942a97bf738641b453afc18
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-10-06 14:08:33 -0300

    feat: add new images

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 
 import { About } from './pages/about'
 import { Partnerships } from './pages/partnerships'
-import { Contact } from './pages/contact'
-import { Footer } from './pages/footer'
 import { Home } from './pages/home'
-import { Main } from './styles'
+
 
 function App() {
 
   return (
     <>
-      <Main>
         <Home />
         <About/>
         {/* <SocialMedia/> */}
         <Partnerships/>
-        <Contact/>
-        <Footer/>
-      </Main>
+        {/* <Contact/> */}
     </>
   )
 }
