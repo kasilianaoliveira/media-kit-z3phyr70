ca4aff81eb5ee7d3660ea2497a6c178d482b82a5
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-10-06 14:09:02 -0300

    feat: add Title component with linear background

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,25 +3,25 @@ import styled from "styled-components";
 
 interface TitleProps {
   title: string;
+  size?: number;
 }
 
-export const Title = ({title}: TitleProps) => {
+export const Title = ({title, size}: TitleProps) => {
   return (
-    <TitleContent>{title}</TitleContent>
+    <TitleContent $size={size}>{title}</TitleContent>
   )
 }
 
 
 
-export const TitleContent = styled.h1`
-
+export const TitleContent = styled.h1<{ $size?: number }>`
   text-transform: uppercase;
-
+  text-wrap: wrap;
   background: radial-gradient(47.96% 976.55% at 53.59% 49.72%, #FFFFFF 0%, #706A6A 100%) /* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   background-clip: text;
-  font-size: 6.25rem;
+  /* font-size:  ; */
+  font-size: clamp(1rem,10vw, ${props => props.$size ? `${props.$size}px` :'6.25rem'});
   font-weight: 400;
-
-`;
+`
